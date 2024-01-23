4f0765062dcac2c034d8a6d26ec5634fb296d628
Si Parker <si@plogic.co.uk>
2024-01-23 11:19:07 +0000

    added: simple info to HTML landing page added: a sprinkle of bootstrap

--- fossa/views/web.py
+++ fossa/views/web.py
@@ -1,12 +1,27 @@
 """
 Non-API views in HTML
 """
-from flask import Blueprint, render_template
+from flask import Blueprint, current_app, render_template
 
 web_views = Blueprint("web", __name__)
 
 
 @web_views.route("/")
 def index():
-    page_vars = {}
+    "Summary page about the compute node"
+
+    governor = current_app.fossa_governor
+    node_info = [
+        ("Node identifier", governor.governor_id),
+        ("Max concurrent tasks", governor.runtime.max_concurrent_tasks),
+        ("Available processing capacity", governor.available_processing_capacity.value),
+    ]
+
+    # Just the details of what to execute. Results to be added here later.
+    previous_tasks = [t["task_spec"] for t in governor.previous_tasks]
+
+    page_vars = {
+        "recent_completed_tasks": previous_tasks,
+        "node_info": node_info,
+    }
     return render_template("web_root.html", **page_vars)
